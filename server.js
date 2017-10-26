07e672dd9ae49948a3998c1a02df688601849d50
Michael Wang <michael.wang@fitbit.com>
2017-10-26 13:06:44 -0700

    Fix heroku port issue

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const request = require('request');
 const parser = require('xml2json');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use("/public", express.static(path.join(__dirname, 'public')));
 
